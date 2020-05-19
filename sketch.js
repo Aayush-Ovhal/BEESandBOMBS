640cb84e351e2a058ac20916a0c18ba5ca43dde4
Aayush-Ovhal <61664556+Aayush-Ovhal@users.noreply.github.com>
2020-05-20 01:08:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+let angle = 0;
+let w = 24;
+let ma;
+let maxD;
+
+function setup(){
+canvas = createCanvas(600,600,WEBGL);
+
+ma = atan(1 / sqrt(2))
+
+maxD = dist(0,0,200,200);
+}
+
+function draw(){
+    background(100);
+   ortho(-450,450,-450,450,0,1000);
+
+    rotateX(-QUARTER_PI);
+    rotateY(ma);
+
+for(let z = 0; z < height; z += w){
+ for(let x = 0; x < width; x += w){
+    push();
+    let d = dist(x,z,width/2,height/2);
+    offset = map(d, 0, maxD, -PI, PI);
+    let a = angle + offset; 
+    let h = map(sin(a),-1,1,100,300);
+    rectMode(CENTER);
+    fill(255);
+    translate(x - width/2, 0, z - height/2);
+    normalMaterial();
+    box(w - 2,h,w - 2);
+    pop();
+  }
+}
+
+    angle -= 0.1;
+
+}
